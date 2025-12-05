93e81fb86ca0802a89f4727a6d94514dde3dcc3b
guromain <romain.guibert44@ynov.com>
2025-12-05 10:33:07 +0100

    Tri.go Tri_anciennete_croissant et Tri_anciennete_decroissant qui tri la liste d'artiste selon leur anciennete

--- Fonction_go/Tri.go
+++ Fonction_go/Tri.go
@@ -17,9 +17,29 @@ func Tri_alpha_decroissant (artistes []Artist) []Artist {
 	for i:=0 ; i<len(artistes)-1 ; i++ {
 		for j:=i+1 ; j<len(artistes) ; j++ {
 			if artistes[i].Nom < artistes[j].Nom {
-				temp := artistes[i]
-				artistes[i] = artistes[j]
-				artistes[j] = temp
+				artistes[i],artistes[j] = artistes[j],artistes[i]
+			}
+		}
+	}
+	return artistes
+}
+
+func Tri_anciennete_croissant (artistes []Artist) []Artist{
+	for i:=0 ; i<len(artistes)-1;i++{
+		for j:=0 ; j<len(artistes);j++{
+			if artistes[i].Annee_deb<artistes[i].Annee_deb{
+				artistes[i],artistes[j] = artistes[j],artistes[i]
+			}
+		}
+	}
+	return artistes
+}
+
+func Tri_anciennete_decroissant (artistes []Artist) []Artist{
+	for i:=0 ; i<len(artistes)-1;i++{
+		for j:=0 ; j<len(artistes);j++{
+			if artistes[i].Annee_deb>artistes[i].Annee_deb{
+				artistes[i],artistes[j] = artistes[j],artistes[i]
 			}
 		}
 	}
