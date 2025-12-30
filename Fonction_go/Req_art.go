b92b344f5727613278c320ca2f210764679b30ea
guromain <romain.guibert44@ynov.com>
2025-12-30 11:55:37 +0100

    Req_art.go Request_art modification pour que la fonction renvoie une erreur et récupère les artistes de manière propre

--- Fonction_go/Req_art.go
+++ Fonction_go/Req_art.go
@@ -3,34 +3,51 @@ package fonction_go
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 )
 
-
-func Request_art(artistes *[]Artist) {
-	url := "https://groupietrackers.herokuapp.com/api/artists"
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	err := json.Unmarshal(body, &artistes)
+func Request_art(artistes *[]Artist) error {
+	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		fmt.Println(err)
+		return fmt.Errorf("appel API artists: %w", err)
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("API artists retourne le statut %d", resp.StatusCode)
+	}
+
+	var payload []Artist
+	if err := json.NewDecoder(resp.Body).Decode(&payload); err != nil {
+		return fmt.Errorf("décodage JSON artists: %w", err)
+	}
+
+	converted := make([]Artist, 0, len(payload))
+	for _, a := range payload {
+		converted = append(converted, Artist{
+			Id:            a.Id,
+			Nom:           a.Nom,
+			Image:         a.Image,
+			Annee_deb:     a.Annee_deb,
+			Date_prem_alb: a.Date_prem_alb,
+			Membres:       a.Membres,
+		})
+	}
+
+	*artistes = converted
+	return nil
 }
 
 func Get_artistes(artistes []Artist) []string {
 	var list_art []string
-	for i:=0 ; i<len(artistes); i++ {
+	for i := 0; i < len(artistes); i++ {
 		list_art = append(list_art, artistes[i].Nom)
 	}
 	return list_art
 }
 
 func Find_artist(artistes []Artist, name string) Artist {
-	for i:=0 ; i<len(artistes); i++ {
+	for i := 0; i < len(artistes); i++ {
 		if artistes[i].Nom == name {
 			return artistes[i]
 		}
