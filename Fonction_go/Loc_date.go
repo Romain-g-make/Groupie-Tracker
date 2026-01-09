25acad1ad7c9e32274b882f31b5f72ab8d310f70
guromain <romain.guibert44@ynov.com>
2026-01-09 09:09:25 +0100

    Loc_Date.go Loc_date qui prend en paramètre une id et a l'aide des fonction get_date et get_loc (modifier pour gérer les erreurs) retourne un dictionnaire avec les locations et les dates liés

--- Fonction_go/Loc_date.go
+++ Fonction_go/Loc_date.go
@@ -2,30 +2,71 @@ package fonction_go
 
 import (
 	"encoding/json"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 func Get_Locations(id int) []string {
+	type locationsResp struct {
+		Id        int      `json:"id"`
+		Locations []string `json:"locations"`
+		DatesLink string   `json:"dates"`
+	}
 
-	var locations []string
-	url := "https://groupietrackers.herokuapp.com/api/locations" + "/" + string(id)
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
+	url := "https://groupietrackers.herokuapp.com/api/locations/" + strconv.Itoa(id)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil
+	}
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	json.Unmarshal(body, &locations)
-	return locations
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return nil
+	}
+
+	var payload locationsResp
+	if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+		return nil
+	}
+	return payload.Locations
 }
 
 func Get_Dates(id int) []string {
+	type datesResp struct {
+		Id    int      `json:"id"`
+		Dates []string `json:"dates"`
+	}
 
-	var dates []string
-	url := "https://groupietrackers.herokuapp.com/api/dates" + "/" + string(id)
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
+	url := "https://groupietrackers.herokuapp.com/api/dates/" + strconv.Itoa(id)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil
+	}
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil
+	}
 	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	json.Unmarshal(body, &dates)
-	return dates
+	if res.StatusCode < 200 || res.StatusCode >= 300 {
+		return nil
+	}
+
+	var payload datesResp
+	if err := json.NewDecoder(res.Body).Decode(&payload); err != nil {
+		return nil
+	}
+	return payload.Dates
+}
+
+func Loc_date(id int) (map[string]string) {
+	locations := Get_Locations(id)
+	dates := Get_Dates(id)
+	result := make(map[string]string)
+	for i:=0 ; i < len(locations) && i < len(dates); i++ {
+		result[locations[i]] = dates[i]
+	}
+	return result
 }
