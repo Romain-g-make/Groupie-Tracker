18415ccae4fb18ef37f14cad15d742b1fb3ecde7
guromain <romain.guibert44@ynov.com>
2026-01-12 10:45:33 +0100

    Search.go getSearchField qui renvoie un tableau de string de données d'un artists pour faire une recherche choisi par l'utilisateur entre date, locations et artistes

--- Fonction_go/Search.go
+++ Fonction_go/Search.go
@@ -5,69 +5,91 @@ import (
 	"strings"
 )
 
-func Search(artistes []Artist, find_art string) map[string]int {
+func getSearchField(artist Artist, searchType string) []string {
+	switch searchType {
+	case "artist":
+		return []string{artist.Nom}
+	case "date":
+		return Get_Dates(artist.Id)
+	case "location":
+		return Get_Locations(artist.Id)
+	default:
+		return []string{}
+	}
+}
+
+func Search(artistes []Artist, find string, searchType string) map[string]int {
 
 	dic_art := make(map[string]int)
-	find_art_lower := strings.ToLower(find_art)
+	findLower := strings.ToLower(find)
 
-	for i := 0; i < len(artistes); i++ {
+	for _, artiste := range artistes {
+
+		field := strings.ToLower(strings.Join(getSearchField(artiste, searchType), " "))
+		
+
 		correspondance := 0
-		nom_lower := strings.ToLower(artistes[i].Nom)
-		if nom_lower == find_art_lower {
-			correspondance = len(find_art) * 3
-		} else if strings.Contains(nom_lower, find_art_lower) {
-			correspondance = len(find_art) * 2
-			if nom_lower[0:1] == find_art_lower[0:1] {
+
+		if field == findLower {
+			correspondance = len(find) * 3
+		} else if strings.Contains(field, findLower) {
+			correspondance = len(find) * 2
+			if field[0:1] == findLower[0:1] {
 				correspondance++
 			}
 		} else {
-			min_len := len(nom_lower)
-			if len(find_art_lower) < min_len {
-				min_len = len(find_art_lower)
+			minLen := len(field)
+			if len(findLower) < minLen {
+				minLen = len(findLower)
 			}
-			for j := 0; j < min_len; j++ {
-				if nom_lower[j] == find_art_lower[j] {
+			for i := 0; i < minLen; i++ {
+				if field[i] == findLower[i] {
 					correspondance++
 				} else {
 					break
 				}
 			}
 		}
+
 		if correspondance > 0 {
-			dic_art[artistes[i].Nom] = correspondance
+			dic_art[artiste.Nom] = correspondance
 		}
 	}
 	return dic_art
 }
 
-func Searching(artistes []Artist, find_art string) []Artist {
+func Searching(artistes []Artist, find string, searchType string) []Artist {
 
 	var result []Artist
 
-	if find_art == "" {
+	if find == "" {
 		return artistes
 	}
-	dic_art := Search(artistes, find_art)
+
+	dic_art := Search(artistes, find, searchType)
+
 	if len(dic_art) == 0 {
-		fmt.Println("Aucun résultat trouvé pour:", find_art)
+		fmt.Println("Aucun résultat trouvé pour:", find)
 		return result
 	}
+
 	for len(dic_art) > 0 {
-		max_correspondance := 0
-		max_key := ""
+		maxScore := 0
+		maxKey := ""
+
 		for key, score := range dic_art {
-			if score > max_correspondance {
-				max_correspondance = score
-				max_key = key
+			if score > maxScore {
+				maxScore = score
+				maxKey = key
 			}
 		}
-		if max_key != "" {
-			artiste := Find_artist(artistes, max_key)
-			if artiste.Nom != "" {
-				result = append(result, artiste)
-			}
-			delete(dic_art, max_key)
+
+		artiste := Find_artist(artistes, maxKey)
+		if artiste.Nom != "" {
+			result = append(result, artiste)
 		}
+		delete(dic_art, maxKey)
 	}
+
 	return result
 }
