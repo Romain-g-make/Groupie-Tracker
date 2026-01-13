ebc8f12554a3ae3b38b34affa9bfb557bb51f274
guromain <romain.guibert44@ynov.com>
2026-01-13 08:53:39 +0100

    Pagination.go Pagination modification pour que la pagination fonctionne proprement

--- Fonction_go/pagination.go
+++ Fonction_go/pagination.go
@@ -1,23 +1,23 @@
 package fonction_go
 
 func Pagination(nb int, artistes []Artist, page int) []Artist {
-    if nb <= 0 || page < 0 || len(artistes) == 0 {
-        return artistes
-    }
-    var result []Artist
+	if nb <= 0 || page < 0 || len(artistes) == 0 {
+		return artistes
+	}
+	var result []Artist
 	for i := 0; i < nb; i++ {
-		result = append(result,artistes[i+(page*nb)])
+		result = append(result, artistes[i+(page*nb)])
 	}
 	return result
 }
 
 func GetTotalPages(nb int, artistes []Artist) int {
-    if nb <= 0 {
-        return 1
-    }
-    total := len(artistes) / nb
-    if len(artistes) % nb != 0 {
-        total++
-    }
-    return total
+	if nb <= 0 {
+		return 1
+	}
+	total := len(artistes) / nb
+	if len(artistes)%nb != 0 {
+		total++
+	}
+	return total
 }
